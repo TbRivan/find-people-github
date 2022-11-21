ba42fe4cc657c973f9d0f153fde7b43947bd8b55
DESKTOP-EC70NAU\User <tbrivan22@gmail.com>
2022-11-21 20:38:41 +0700

    Create a Dark mode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Switcher from "./Switcher";
 
 export default function Footer() {
   return (
     <div className="text-center p-10 border-t text-sm md:text-md">
       Search GIT Username using <span className="font-bold">Next.JS</span>
+      <Switcher />
     </div>
   );
 }
--- pages/layout.tsx
+++ pages/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
-      <div className="flex min-h-screen">
+      <div className="flex min-h-screen bg-white dark:bg-gray-900">
         <main className="m-auto w-4/5 md:w-2/3 lg:w-1/3">{children}</main>
       </div>
       <Footer />
